2be5c6adfbc9ffe243510a43127a23e966784e56
Kyle S <kspa@vt.edu>
2024-07-06 19:12:07 -0400

    Progress on endpoints

--- lib/Endpoints.ts
+++ lib/Endpoints.ts
@@ -1,5 +1,6 @@
 const BASE = 'https://api.dci.org/api/v1/';
 
-const COMPETITIONS =  (season: string) => `${BASE}/competitions?${season}`;
-const COMPETITION = (season: string, competition: string) => `${BASE}/competitions?${season}/${competition}`;
-
+const COMPETITIONS =  (season?: string, competition?: string) => `${BASE}/competitions${season !== undefined ? `?season=${season}` : ''}${competition !== undefined ? `/${competition}` : ''}`;
+const CORPS = (name?: string) => `${BASE}/corps${name!== undefined ? `/${name}` : ''}`;
+const EVENTS = (event?: string) => `${BASE}/events${event !== undefined ? `/${event}` : ''}`;
+const SPONSORS = (sponsor?: string) => `${BASE}/sponsors${sponsor !== undefined ? `/${sponsor}` : ''}`;
--- lib/interfaces/API.ts
+++ lib/interfaces/API.ts
@@ -1,20 +1,81 @@
 export interface Corps {
+    id: string;
     name: string;
-    directors: string[];
-    founded: Date;
-    championships: number;
-    last_score: number;
+    type: string;
+    website: string;
+    facebook?: string;
+    twitter?: string;
+    youtube?: string;
+    about: string;
+    status: string;
+    displayCity: string;
+    corpsLogo: string;
+    corpsPhoto: string;
+    corpsMMDLLinkAudio: string;
+    corpsMMDLLinkVideo: string;
+    entityType: string;
+    slug: string;
+    auditions: string;
 };
 
-export interface Competition {
+export interface Event {
+    id: string;
     name: string;
-    location: string;
-    date: Date;
+    slug: string;
+    locationCity: string;
+    locationState: string;
+    timezone: string;
+    tickets: string;
+    ticketsOnSale: string;
+    sponsor: string;
+    livestream: string;
+    thumbnail: string;
+    startDate: string;
+    startTime: string;
     schedules: Schedule[];
+    venue: Venue;
+    image: string;
+    ticketingMapImage: string;
+    googleMapsImage: string;
 }
 
 export interface Schedule {
     unitName: string;
     displayCity: string;
     time: string;
+}
+
+export interface Venue {
+    name: string;
+    address: string;
+    zipcode: string;
+    fieldHashmarks: boolean;
+    fieldHashmarksType: string;
+    goalPosts: boolean;
+    fieldElectricity: boolean;
+    americanFlagLocation: string;
+    tunnelHeight: string;
+    videoBoard: boolean;
+    accessToBoxOffice: boolean;
+    airConditioning: boolean;
+    clearBagVenue: boolean;
+    marketplaceLocation: string;
+    bagPolicy: string;
+    spectatorEntrance: string;
+    spectatorReEntry: string;
+    willCallLocation: string;
+    concessions: string;
+    ambulanceLocation: string;
+    micsOnField: string;
+    soundOrdinance?: string;
+    ticketTakers: string;
+    boxOfficeVolunteers: boolean;
+    ushers: string;
+    security: string;
+    seatNumbering: false;
+    seatSize: string;
+    marketplaceType: string;
+    marketplaceElectricity: string;
+    bagPolicyDescription: string;
+    cashless: boolean;
 }
